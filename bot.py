33c31e6b9f85ce58a14d5b2e6c5d598cd3d9f762
yamdereneko <yamdereneko@icloud.com>
2023-03-30 10:56:56 +0800

    Changes

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ nonebot.load_from_toml("pyproject.toml")
 
 logger.add("logs/error.log")
 
+
 if __name__ == "__main__":
     nonebot.logger.warning("请使用指令[nb run]来运行此项目!")
     nonebot.run(app="__main__:app", port=8080)
